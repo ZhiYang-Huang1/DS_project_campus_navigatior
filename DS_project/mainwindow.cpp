ae6205564ad46405b031c5c74fa630f6fc52eb1d
HuangZY <3153580592@qq.com>
2024-11-10 17:00:57 +0800

    更新

--- DS_project/mainwindow.cpp
+++ DS_project/mainwindow.cpp
@@ -1,11 +1,12 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include "info_dialog.h"
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    initUI();
 }
 
 MainWindow::~MainWindow()
@@ -13,3 +14,611 @@ MainWindow::~MainWindow()
     delete ui;
 
 }
+
+
+void MainWindow::initUI()
+{
+    ui->CB_departure->addItem("大学城校区(1-16)");
+    ui->CB_departure->addItem("国际校区(17-33)");
+    ui->CB_destination->addItem("大学城校区(1-16)");
+    ui->CB_destination->addItem("国际校区(17-33)");
+
+
+}
+
+
+void MainWindow::on_btn1_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+
+
+}
+
+
+void MainWindow::on_CB_departure_currentTextChanged(const QString &arg1)
+{
+    if (arg1=="大学城校区(1-16)")
+    {
+        QRegExp ediName("^(?:[1-9]|1[0-6])$");
+        ui->departureEdit->setValidator(new QRegExpValidator(ediName,this));
+        ui->departureEdit->setPlaceholderText("请输入1-16的数字");
+    }
+    else if (arg1=="国际校区(17-33)")
+    {
+        QRegExp ediName("^(?:1[7-9]|2[0-9]|3[0-3])$");
+        ui->departureEdit->setValidator(new QRegExpValidator(ediName,this));
+        ui->departureEdit->setPlaceholderText("请输入17-33的数字");
+    }
+
+
+}
+
+
+void MainWindow::on_CB_destination_currentTextChanged(const QString &arg1)
+{
+    if (arg1=="大学城校区(1-16)")
+    {
+        QRegExp ediName("^(?:[1-9]|1[0-6])$");
+        ui->destinationEdit->setValidator(new QRegExpValidator(ediName,this));
+        ui->destinationEdit->setPlaceholderText("请输入1-16的数字");
+    }
+    else if (arg1=="国际校区(17-33)")
+    {
+        QRegExp ediName("^(?:1[7-9]|2[0-9]|3[0-3])$");
+        ui->destinationEdit->setValidator(new QRegExpValidator(ediName,this));
+        ui->destinationEdit->setPlaceholderText("请输入17-33的数字");
+    }
+
+}
+
+
+void MainWindow::on_btn2_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn3_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn4_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+void MainWindow::on_btn5_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+void MainWindow::on_btn6_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn7_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn8_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn9_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn10_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn11_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn12_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn13_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn14_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn15_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn16_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn17_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn18_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn19_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn20_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn21_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn22_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn23_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn24_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn25_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn26_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn27_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn28_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn29_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn30_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn31_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn32_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
+
+void MainWindow::on_btn33_clicked()
+{
+    QDialog *infoWindow = new info_dialog(this);
+    QTextBrowser *textBrowser = infoWindow->findChild<QTextBrowser *>("textBrowser");
+
+    QPixmap *img = new QPixmap;
+    img->load(":/images/administration_building.jpeg");                             // 加载照片
+    QLabel *label = infoWindow->findChild<QLabel *>("label");                    // 获取子窗口label部件
+
+    label->setPixmap(*img);
+        // 设置介绍图片
+    textBrowser->setText(tr("C12到C17宿舍"));    // 设置介绍文字
+
+    infoWindow->exec();
+}
+
--- DS_project/mainwindow.h
+++ DS_project/mainwindow.h
@@ -2,6 +2,13 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QPainter>
+#include <QMessageBox>
+#include <QMouseEvent>
+#include <QCloseEvent>
+#include <QDialog>
+#include <QRegExpValidator>
+#include <QDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -17,7 +24,81 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_btn1_clicked();
+
+    void on_CB_departure_currentTextChanged(const QString &arg1);
+
+    void on_CB_destination_currentTextChanged(const QString &arg1);
+
+    void on_btn2_clicked();
+
+    void on_btn3_clicked();
+
+    void on_btn4_clicked();
+
+    void on_btn5_clicked();
+
+    void on_btn6_clicked();
+
+    void on_btn7_clicked();
+
+    void on_btn8_clicked();
+
+    void on_btn9_clicked();
+
+    void on_btn10_clicked();
+
+    void on_btn11_clicked();
+
+    void on_btn12_clicked();
+
+    void on_btn13_clicked();
+
+    void on_btn14_clicked();
+
+    void on_btn15_clicked();
+
+    void on_btn16_clicked();
+
+    void on_btn17_clicked();
+
+    void on_btn18_clicked();
+
+    void on_btn19_clicked();
+
+    void on_btn20_clicked();
+
+    void on_btn21_clicked();
+
+    void on_btn22_clicked();
+
+    void on_btn23_clicked();
+
+    void on_btn24_clicked();
+
+    void on_btn25_clicked();
+
+    void on_btn26_clicked();
+
+    void on_btn27_clicked();
+
+    void on_btn28_clicked();
+
+    void on_btn29_clicked();
+
+    void on_btn30_clicked();
+
+    void on_btn31_clicked();
+
+    void on_btn32_clicked();
+
+    void on_btn33_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+private:
+    void initUI();
 };
 #endif // MAINWINDOW_H
